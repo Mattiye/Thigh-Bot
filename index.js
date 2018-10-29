81d34e78fc06a6dc515ca944a017a04e88f78bb9
TheOppaiGuy <theoppaiguydev@gmail.com>
2018-10-30 00:31:10 +1100

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const botConfig = require("./botConfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", () =>{
+  console.log("Bot is Ready");
+});
+
+bot.on("guildMemberAdd", member => {
+  member.addRole(member.guild.roles.find("name", "<----------GENDER----------->"));
+  member.addRole(member.guild.roles.find("name", "<-------------AGE------------->"));
+  member.addRole(member.guild.roles.find("name", "<---------SEXUALITY-------->"));
+  member.addRole(member.guild.roles.find("name", "<------RELATIONSHIP------->"));
+  member.addRole(member.guild.roles.find("name", "<-----------OTHER------------>"));
+});
+
+bot.login(botConfig.token);
